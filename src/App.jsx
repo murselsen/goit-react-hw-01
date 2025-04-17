a10ebdfc38133082ead77447e949df62129e65dd
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-18 02:18:12 +0300

    Refactor App component: integrate FriendList and user data, remove FriendListItem component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,13 @@
 // @murselsen
 import React from 'react'
+// Css
 import './App.css'
 // Components
 import Profile from './components/Profile'
-
-const userData = {
-  username: "Jacques Gluke",
-  tag: "jgluke",
-  location: "Ocho Rios, Jamaica",
-  avatar: "https://cdn-icons-png.flaticon.com/512/2922/2922506.png",
-  stats: {
-    followers: 5603,
-    views: 4827,
-    likes: 1308
-  }
-};
-const friends = [
-  {
-    "avatar": "<https://cdn-icons-png.flaticon.com/512/1998/1998592.png>",
-    "name": "Mango",
-    "isOnline": true,
-    "id": 1812
-  },
-  {
-    "avatar": "<https://cdn-icons-png.flaticon.com/512/616/616438.png>",
-    "name": "Kiwi",
-    "isOnline": false,
-    "id": 1137
-  },
-  {
-    "avatar": "<https://cdn-icons-png.flaticon.com/512/1623/1623681.png>",
-    "name": "Ajax",
-    "isOnline": true,
-    "id": 1213
-  },
-  {
-    "avatar": "<https://cdn-icons-png.flaticon.com/512/2977/2977285.png>",
-    "name": "Jay",
-    "isOnline": true,
-    "id": 1714
-  },
-  {
-    "avatar": "<https://cdn-icons-png.flaticon.com/512/1998/1998749.png>",
-    "name": "Poly",
-    "isOnline": false,
-    "id": 1284
-  }
-];
-
+import FriendList from './components/FriendList'
+// Data
+import userData from './userData.json'
+import friends from './friends.json'
 function App() {
 
   return (
---
+++ src/components/FriendList.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import FriendListCss from './FriendList.module.css';
+
+
+const FriendListItem = ({
+    avatar,
+    // @murselsen
+    name,
+    isOnline
+}) => {
+    const statusValue = isOnline ? "Online" : "Offline";
+    const statusColor = isOnline ? "green" : "red";
+    return (
+        <div className={FriendListCss.FriendList__card}>
+            <img className={FriendListCss.FriendList__card_image} src={avatar} alt={name} width="48" />
+            <p className={FriendListCss.FriendList__card_name}>{name}</p>
+            <p className={FriendListCss.FriendList__card_state} style={{ color: statusColor }}>{statusValue}</p>
+        </div>
+
+    )
+};
+
+const FriendList = ({ friends }) => {
+    const mapFriends = friends.map(friend => <li className={FriendListCss.FriendList__item} > <FriendListItem key={friend.key} avatar={friend.avatar} name={friend.name} isOnline={friend.isOnline} /> </li>)
+    return (
+        <ul className={FriendListCss.FriendList}>
+            {/* @murselsen */}
+            {mapFriends}
+        </ul>
+    );
+};
+
+export default FriendList;
