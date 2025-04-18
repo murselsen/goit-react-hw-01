641b3cf27c1327ba0e76b119ac7918a4bcc7848e
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-18 12:48:20 +0300

    Remove unnecessary comments in index.html and main.jsx for cleaner code

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
- 
+//  @murselsen
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
