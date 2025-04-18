799ca04c291687e3b1a3f2a2ecb67a8ad2108d0a
Mürsel SEN <murselsen_35@hotmail.com>
2025-04-18 14:17:26 +0300

    Remove unused environment variables and comments from vite.config.js for cleaner configuration

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import process from "process";
-
-const appName = process.env.npm_package_name;
-const appMode = process.env.NODE_ENV; // Development or Production
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  //uygulama yolu
 
-  // @murselsen
-  base: appMode === "development" ? "/" : `/${appName}/`,
   // @murselsen
   server: {
     port: 3000,
